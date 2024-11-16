bf36e0b243bfd3345dc3f7a935e23fcf26da14d9
babayigit <babayigit@gmail.com>
2024-11-16 18:30:46 +0300

    merhaba

--- src/day05/P04_array.java
+++ src/day05/P04_array.java
@@ -1,5 +1,7 @@
 package day05;
 
+import java.util.Arrays;
+
 public class P04_array {
     public static void main(String[] args) {
         /*
@@ -8,6 +10,17 @@ public class P04_array {
         output:{4,36,16,25,64,81}
 
          */
+        int [] arr={2,6,4,5,8,9,10,64};
+
+        int [] squares=new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            squares[i]=arr[i]*arr[i];
+        }
+        System.out.println("Elemanlar = "+Arrays.toString(arr));
+        System.out.println("Elemanların Kareleri = " + Arrays.toString(squares));
+
+
 
     }
 }
---
+++ src/day05/P05_array.java
@@ -0,0 +1,7 @@
+package day05;
+
+public class P05_array {
+    public static void main(String[] args) {
+       // String bir parametre kabul eden ve rakamların toplamını yazdıran bir method yazın
+    }
+}
